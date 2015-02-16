118a21d2f4c0b0d2fc5a5dc6e18dd85d3a07e871
Arnaud Delpeyroux <arnaud.delpeyroux@etu.u-bordeaux.fr>
2015-02-16 12:19:31 +0100

    implementation model : arnaud

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -1,7 +1,122 @@
 package model;
 
+import java.awt.Color;
+
+import view.EdgeView;
+
 public class Edge {
+	
 	private Vertex srcVertex;
+	
 	private Vertex dstVertex;
+	
 	private EdgeStyle style;
+	
+	private EdgeView mainView;
+	
+	private Graph graph;
+	
+	/**
+	 * 
+	 */
+	public boolean isTurnedToSource;
+		
+	/**
+	 * 
+	 */
+	public boolean isTurnedToDest;
+	
+	// Setter and getter for field : style
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public EdgeStyle getStyle() {
+		return style;
+	}
+
+	/**
+	 * 
+	 * @param style
+	 */
+	public void setStyle(EdgeStyle style) {
+		this.style = style;
+	}
+
+	//Setter and getter for field : mainView
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public EdgeView getMainView() {
+		return mainView;
+	}
+
+	/**
+	 * 
+	 * @param mainView
+	 */
+	public void setMainView(EdgeView mainView) {
+		this.mainView = mainView;
+	}
+
+	// Modification methods for fields in style
+	
+	public Color getColor()
+	{
+		return style.color;
+	}
+	
+	public void setColor(Color color)
+	{
+		//TODO
+	}
+	
+	public float getThickness()
+	{
+		return style.thickness;
+	}
+	
+	public void setThickness(float thickness)
+	{
+		//TODO
+	}
+	
+	public String getLabel()
+	{
+		return mainView.getLabel();
+	}
+	
+	public void setLabel(String label)
+	{
+		mainView.setLabel(label);s
+	}
+	
+	public Color getTextColor()
+	{
+		return style.color;
+	}
+	
+	public void setTextColor(Color textColor)
+	{
+		//TODO
+	}
+	
+	public Font getFont()
+	{
+		return style.font;
+	}
+	
+	public void setFont(Font font)
+	{
+		//TODO
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/model/EdgeStyle.java
+++ src/model/EdgeStyle.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.awt.Color;
+import java.awt.Font;
+
+
 public class EdgeStyle {
+	
+	public float thickness;
+	public Color color; 
+	public Color textColor;
+	public Font font;
 
 }
