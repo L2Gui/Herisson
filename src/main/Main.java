643b4713c3f6321c585fb1386d13cb89ccc20a5b
clejacquet <clem.jacquet@gmail.com>
2015-02-22 13:14:07 +0100

    Simplification du canvas dans le main (afficher le quad rajoute du code pour pas grand chose, je l'ai donc enlevé) + ajout de getters / setters pour EdgeStyle

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,62 +7,22 @@ import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
 import opengl.GLCanvas;
-import opengl.GLObject;
-import opengl.GLShader;
-import opengl.GLTexture;
-import opengl.GLTexturedVertex;
-
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.GLContext;
 
 public class Main {
 	public JFrame frame;
 	
     public static void main(String args[]) {
         Main l10 = new Main();
-        l10.run();
+        try {
+        	l10.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
     
-    public void run() {
-    	try {
-        	createWindow();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-    	
-    	int[] indices = {
-			0, 1, 2,
-			2, 3, 0
-		};
-		
-		GLTexturedVertex v0 = new GLTexturedVertex(); 
-		v0.setXYZ(-0.5f, 0.5f, -1.0f); v0.setRGB(1, 0, 0); v0.setST(0, 0);
-		GLTexturedVertex v1 = new GLTexturedVertex(); 
-		v1.setXYZ(-0.5f, -0.5f, -1.0f); v1.setRGB(0, 1, 0); v1.setST(0, 1);
-		GLTexturedVertex v2 = new GLTexturedVertex(); 
-		v2.setXYZ(0.5f, -0.5f, -1.0f); v2.setRGB(0, 0, 1); v2.setST(1, 1);
-		GLTexturedVertex v3 = new GLTexturedVertex(); 
-		v3.setXYZ(0.5f, 0.5f, -1.0f); v3.setRGB(1, 1, 1); v3.setST(1, 0);
-		
-		GLTexturedVertex[] vertices = new GLTexturedVertex[] {v0, v1, v2, v3};
-		
-		try {
-			GLCanvas canvas = new GLCanvas();
-			GLShader shader = new GLShader("texture.vert", "texture.frag");
-			
-			GLTexture texture = new GLTexture("resource/png/ash_uvgrid04.png");
-			GLObject obj = new GLObject(shader, texture, vertices, indices);
-			
-			frame.getContentPane().add(canvas);
-			
-			canvas.add(shader);
-			canvas.add(texture);
-			canvas.add(obj);
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-		}
+    public void run() throws Exception {
+        createWindow();
 		
 		frame.setVisible(true);
     }
@@ -123,5 +83,9 @@ public class Main {
 			menu.add(display);
     		
     	frame.setJMenuBar(menu);
+    	
+    	GLCanvas canvas = new GLCanvas();
+		
+		frame.getContentPane().add(canvas);
     }
 }
--- src/model/EdgeStyle.java
+++ src/model/EdgeStyle.java
@@ -11,37 +11,44 @@ public class EdgeStyle {
 	private Color textColor;
 	private Font font;
 	private LineStyle style;
+	
 	public float getThickness() {
 		return thickness;
 	}
+	
 	public void setThickness(float thickness) {
 		this.thickness = thickness;
 	}
+	
 	public Color getColor() {
 		return color;
 	}
+	
 	public void setColor(Color color) {
 		this.color = color;
 	}
+	
 	public Color getTextColor() {
 		return textColor;
 	}
+	
 	public void setTextColor(Color textColor) {
 		this.textColor = textColor;
 	}
+	
 	public Font getFont() {
 		return font;
 	}
+	
 	public void setFont(Font font) {
 		this.font = font;
 	}
+	
 	public LineStyle getStyle() {
 		return style;
 	}
+	
 	public void setStyle(LineStyle style) {
 		this.style = style;
 	}
-	
-	
-
 }
