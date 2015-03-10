43a0a00b7462fa6285d772a74e7dc3268e7d7919
Arnaud Delpeyroux <arnaud.delpeyroux@etu.u-bordeaux.fr>
2015-03-10 23:18:25 +0100

    Ajout StyleManager + interface Style, implementation de quelques methodes du model

--- src/main/java/model/EdgeStyle.java
+++ src/main/java/model/EdgeStyle.java
@@ -4,13 +4,14 @@ import java.awt.Color;
 import java.awt.Font;
 
 
-public class EdgeStyle {
+public class EdgeStyle implements IStyle {
 	
 	private float thickness;
 	private Color color; 
 	private Color textColor;
 	private Font font;
 	private LineStyle style;
+    private int usageCount;
 	
 	public float getThickness() {
 		return thickness;
@@ -51,4 +52,14 @@ public class EdgeStyle {
 	public void setStyle(LineStyle style) {
 		this.style = style;
 	}
+
+    @Override
+    public int getUsageCount() {
+        return usageCount;
+    }
+
+    @Override
+    public void setUsageCount(int count) {
+        this.usageCount = count;
+    }
 }
---
+++ src/main/java/model/IStyle.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by Arnaud on 10/03/2015.
+ */
+public interface IStyle {
+     public int getUsageCount();
+    public void setUsageCount(int count);
+}
