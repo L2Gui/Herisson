68be0d40f670e6f4072be70ecce613e35b435eb8
Corentin Bienassis <corentin.bienassis@etu.u-bordeaux.fr>
2015-03-13 18:28:39 +0100

    range1000fois

--- src/main/java/view/VertexView.java
+++ src/main/java/view/VertexView.java
@@ -1,14 +1,36 @@
 package view;
 
 import model.Vertex;
+import opengl.GLCanvas;
+import opengl.resource.GLShader;
+import opengl.resource.object.GLObjectUsage;
+import opengl.resource.object.camera.GLPerspectiveCamera;
+import opengl.resource.object.mesh.GLColoredMesh;
+import opengl.resource.object.mesh.GLMesh;
+import opengl.vertex.GLColoredVertex;
 import org.lwjgl.util.vector.Matrix4f;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VertexView extends ViewElement {
 	private Vertex model;
+    private GLColoredMesh mesh;
+    GLShader shader;
 
     public VertexView() {
 
     }
+
+    public VertexView(Vertex model, GLColoredMesh mesh, GLShader shader) {
+        this.mesh = mesh;
+        this.shader = shader;
+        this.model = model;
+    }
+
+
+
 
     public Vertex getModel() {
         return model;
@@ -22,45 +44,8 @@ public class VertexView extends ViewElement {
 	public void render(Matrix4f projectionViewMatrix)
 	{
         super.render(projectionViewMatrix);
-        /*
-        GLShader colorShader = new GLShader("color3D.vert", "color.frag"); //vert pour vertex, pas pour la couleur vert
 
-        List<GLColoredVertex> vertices = new ArrayList<GLColoredVertex>();
-
-        GLColoredVertex v0 = new GLColoredVertex();
-        GLColoredVertex v1 = new GLColoredVertex();
-        GLColoredVertex v2 = new GLColoredVertex();
-        GLColoredVertex v3 = new GLColoredVertex();
-
-        v0.setPosition(-0.5f, -0.5f, 0.0f);
-        v1.setPosition(0.5f, -0.5f, 0.0f);
-        v2.setPosition(-0.5f, 0.5f, 0.0f);
-        v3.setPosition(0.5f, 0.5f, 0.0f);
-
-        v0.setColor(Color.red);
-        v1.setColor(Color.red);
-        v2.setColor(Color.red);
-        v3.setColor(Color.red);
-
-        vertices.add(v0);
-        vertices.add(v1);
-        vertices.add(v2);
-        vertices.add(v3);
-
-        int[] indices = {
-                0, 1, 3,
-                0, 2, 3
-        };
-
-        mesh.setup(vertices, indices, GLObjectUsage.STATIC);
-
-        drawableObject.setShader(colorShader);
-        drawableObject.setMesh(this.mesh);
-
-        app.getCanvas().addResource(colorShader);
-        app.getCanvas().addResource(this.mesh);
-        this.drawableObject.render(projectionViewMatrix);
-
-        System.out.println("paint appelée");*/
+        this.setShader(shader);
+        this.setMesh(this.mesh);
 	}
 }
