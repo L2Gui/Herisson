785197246895e1334e0f500b433359e0ecf2db1a
Louis Deguillaume <louis.deguillaume@etu.u-bordeaux.fr>
2015-03-14 10:38:59 +0100

    début de la fonction de test de capacité ogl

--- src/main/java/opengl/resource/GLShader.java
+++ src/main/java/opengl/resource/GLShader.java
@@ -2,6 +2,7 @@ package opengl.resource;
 
 import static org.lwjgl.opengl.GL20.*;
 
+import org.lwjgl.opengl.ContextCapabilities;
 import org.lwjgl.opengl.GL20;
  
 
@@ -32,19 +33,31 @@ public class GLShader implements IGLResource {
 		this.fragmentPath = fragmentPath;
 		this.isInitialized = false;
 	}
-	
+	public void testCapabilities(){
+        ContextCapabilities cp = GLContext.getCapabilities();
+        System.out.println("Shaders: "+cp.GL_AMD_vertex_shader_layer+ " "+cp.GL_AMD_multi_draw_indirect+" "+cp.GL_AMD_sample_positions+" "+cp.GL_AMD_vertex_shader_tessellator+" "+cp.GL_AMD_vertex_shader_viewport_index);
+        System.out.println("opengl 44: "+cp.OpenGL44);
+        System.out.println("opengl 40: "+cp.OpenGL40);
+        System.out.println("opengl 32: "+cp.OpenGL32);
+        System.out.println("opengl 20: "+cp.OpenGL20);
+        System.out.println("opengl 15: "+cp.OpenGL15);
+        System.out.println("opengl 12: "+cp.OpenGL12);
+        System.out.println("opengl 11: "+cp.OpenGL11);
+    }
 	@Override
 	public void init() {
 		if (this.isInitialized() || this.vertexPath == null || this.fragmentPath == null) {
 			System.err.println("Paths are invalid or this shader is already initialized");
 			return;
 		}
-		
-		if (GLContext.getCapabilities().OpenGL32) {
+
+        testCapabilities();
+
+        if (GLContext.getCapabilities().OpenGL32) {
 			this.vertexPath = "resource/shader/" + this.vertexPath;
 			this.fragmentPath = "resource/shader/" + this.fragmentPath;
 		} else {
-			this.vertexPath = "resource/shader/1.2/" + this.vertexPath;
+            this.vertexPath = "resource/shader/1.2/" + this.vertexPath;
 			this.fragmentPath = "resource/shader/1.2/" + this.fragmentPath;
 		}
 		
