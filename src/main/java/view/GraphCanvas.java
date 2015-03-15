b6fe6ab89302d1725674b7fa25aa26b8f76a023c
clejacquet <clem.jacquet@gmail.com>
2015-03-15 01:06:03 +0100

    Imports qui ont été supprimés rajoutés

--- src/main/java/view/GraphCanvas.java
+++ src/main/java/view/GraphCanvas.java
@@ -2,10 +2,12 @@ package view;
 
 import model.Edge;
 import model.Graph;
+import model.Vertex;
 import opengl.GLCanvas;
+import opengl.resource.GLShader;
+import opengl.resource.object.GLObjectUsage;
 import opengl.resource.object.camera.GLPerspectiveCamera;
 import opengl.resource.object.mesh.GLColorVariantMesh;
-import opengl.resource.object.mesh.GLMesh;
 import opengl.utils.GLRay;
 import opengl.vertex.GLVertex;
 import org.lwjgl.LWJGLException;
