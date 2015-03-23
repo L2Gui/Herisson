900a93adcf9e98adf4b6fba7e44d563621b63c14
Louis Deguillaume <louis.deguillaume@gmail.com>
2015-03-23 01:46:12 +0100

    ajout de fx pour maj edgeview dans graphview

--- src/main/java/controller/action/EditVertexNowAction.java
+++ src/main/java/controller/action/EditVertexNowAction.java
@@ -213,7 +213,7 @@ public class EditVertexNowAction extends MenuAction{
 
         p.add(panelCancel);
         p.add(panelOk);
-        
+
         f.setContentPane(p);
 
         f.setLocationRelativeTo(null);
